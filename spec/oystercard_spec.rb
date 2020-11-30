49088c39731f414fc2d0e500ab6a3076c1010e5c
Aman Tank <amantank187@gmail.com>
2020-11-30 15:50:12 +0000

    Added oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  card = Oystercard.new
+  it "Should be an instance of the Oystercard class" do
+    expect(card).to be_instance_of Oystercard
+  end
+
+  it "Checks if the new card has a 0 balance" do
+    expect(card.balance).to eq 0
+  end
 end
