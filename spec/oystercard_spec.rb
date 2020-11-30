4fae43029159da8990dfba6ec8e99d5beb01c2e6
Chris Whitehouse <chris_whitehouse@hotmail.com>
2020-11-30 16:08:15 +0000

    Step 5: Top Up Functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,17 @@ describe Oystercard do
     expect(card).to be_instance_of Oystercard
   end
 
-  it "Checks if the new card has a 0 balance" do
-    expect(card.balance).to eq 0
+  describe '#balance' do
+    it "Checks if the new card has a 0 balance" do
+      expect(card.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'should add amount to balance' do
+      expect{ card.top_up(5) }.to change{ card.balance }.by 5
+    end
   end
 end
