8dcab43c390425134ad36dda4bc0db5e3644df3a
Aman Tank <amantank187@gmail.com>
2020-11-30 16:49:31 +0000

    Step 6: Adding balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
   attr_reader :balance
+  BALANCE_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Can't exceed the limit of £#{BALANCE_LIMIT}" if @balance + amount > BALANCE_LIMIT 
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,14 +1,14 @@
 require 'oystercard'
 
 describe Oystercard do
-  card = Oystercard.new
+
   it "Should be an instance of the Oystercard class" do
-    expect(card).to be_instance_of Oystercard
+    expect(subject).to be_instance_of Oystercard
   end
 
   describe '#balance' do
     it "Checks if the new card has a 0 balance" do
-      expect(card.balance).to eq 0
+      expect(subject.balance).to eq 0
     end
   end
 
@@ -16,7 +16,13 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'should add amount to balance' do
-      expect{ card.top_up(5) }.to change{ card.balance }.by 5
+      expect{ subject.top_up(5) }.to change{ subject.balance }.by 5
+    end
+
+    it 'Throws an exception if balance limit is exceeded' do
+      balance_limit = Oystercard::BALANCE_LIMIT
+      subject.top_up(balance_limit)
+      expect{ subject.top_up(1) }.to raise_error "Can't exceed the limit of £#{balance_limit}"
     end
   end
 end
